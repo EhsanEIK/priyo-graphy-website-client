af58994397cc775b7f7ec88a4ea206f10f33284f
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-07 22:56:24 +0600

    react-router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './Pages/Shared/Header/Header';
+import { router } from './Routes/Routes/routes';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
