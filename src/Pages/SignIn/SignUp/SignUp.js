72dcffdc0ce2a08005999a3c5f203ae4388ac155
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-08 00:50:24 +0600

    added handleSignUp function and created new user using firebase

--- src/Pages/SignIn/SignUp/SignUp.js
+++ src/Pages/SignIn/SignUp/SignUp.js
@@ -1,11 +1,33 @@
 import { Button, Label, TextInput } from 'flowbite-react';
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const SignUp = () => {
+    const { createUser } = useContext(AuthContext);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        setErrorMsg('');
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                toast.success("User created successfully");
+                form.reset();
+            })
+            .catch(error => setErrorMsg(error.message));
+    }
+
     return (
         <div className='w-1/4 mx-auto'>
-            <form className="flex flex-col border border-gray-200 rounded-lg shadow-lg p-10 gap-4">
+            <form onSubmit={handleSignUp} className="flex flex-col border border-gray-200 rounded-lg shadow-lg p-10 gap-4">
                 <h1 className='text-3xl font-bold text-center mb-5'>Sign Up</h1>
                 <div>
                     <div className="mb-2 block">
@@ -56,6 +78,7 @@ const SignUp = () => {
                     Sign up
                 </Button>
                 <p className='text-sm text-center text-gray-500'>Already have an account?<Link to='/signin' className='hover:underline hover:text-orange-500 ml-1'>Sign in</Link></p>
+                <p className='text-red-600 text-xl text-center'>{errorMsg}</p>
             </form>
         </div>
     );
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,22 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import app from '../../firebase/firbase.config';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 
 export const AuthContext = createContext();
+const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
-    const authInfo = {};
+    const [user, setUser] = useState();
+
+    // create user
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = {
+        user,
+        createUser,
+    };
 
     return (
         <AuthContext.Provider value={authInfo}>
