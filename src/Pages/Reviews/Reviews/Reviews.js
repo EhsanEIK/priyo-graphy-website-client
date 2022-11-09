556eef9c1c674f23ed90628e2c529570af962d2c
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-09 22:47:33 +0600

    added responsive design in Reviews.js file

--- src/Pages/Reviews/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews/Reviews.js
@@ -4,6 +4,7 @@ import Review from './Review';
 const Reviews = ({ service }) => {
     const { _id } = service;
 
+    // get all reviews
     const [reviews, setReviews] = useState([]);
     useEffect(() => {
         fetch(`https://priyo-graphy-server.vercel.app/reviews/services/${_id}`)
@@ -12,7 +13,7 @@ const Reviews = ({ service }) => {
     }, [reviews])
 
     return (
-        <div className='grid grid-cols-3 gap-5 gap-y-10'>
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 gap-y-10 md:mx-0 mx-3'>
             {
                 reviews.map(review => <Review
                     key={review._id}
